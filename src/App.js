997e99e66b3f57526701526ebff6505252a8c23f
Signupz <ludovic.mazet@gmail.com>
2019-12-09 18:33:56 +0100

    Creation de la navbar sous Reactstrap, mise en place du menu burger en bas a droite de l'écran

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import NavbarTest from './components/navbar'
 import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
+      <NavbarTest />
       <Header />
-      
     </div>
   );
 }
