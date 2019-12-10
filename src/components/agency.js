166ed232e4c5d4a37554812b6cfc310fefbbc085
Signupz <ludovic.mazet@gmail.com>
2019-12-10 19:30:25 +0100

    Modification mineures

--- src/components/agency.js
+++ src/components/agency.js
@@ -7,7 +7,7 @@ export default function Agency() {
         <div className="SectionContainer">
             <h1 className="title">L'AGENCE</h1>
             <CarrouselAgency />
-            <p className="aboutAgency">Conçu comme une agence de design, le tattoo shop PIQUEUR DE RUE se veut être à l'écoute des clients et offrir un suivi de qualité.
+            <p className="aboutAgency"><h2>"Une agence de tatouage au service de vos projets"</h2><br/>Conçu comme une agence de design, le tattoo shop PIQUEUR DE RUE se veut être à l'écoute des clients et offrir un suivi de qualité.
             Vous rentrez, vous vous asseyez et nous élaborons ensemble la meilleure approche de votre projet tattoo autour d'un café. Nous tenterons de répondre à toutes vos questions et vous expliquerons toutes les étapes de réalisation de votre projet.<br/><br/>
             Pour que vous puissiez avoir le meilleur recul sur ce que pourra rendre votre tatouage une fois terminé, nous réaliserons une simulation à l'aide de photos de vous "type studio" qui seront prises dans le shop.
             Une fois le projet validé, et votre tatouage terminé, vous repartirez avec un "paper bag" contenant la plaquette de soins une crème pour vos premières semaines ainsi qu’une collation.</p>
