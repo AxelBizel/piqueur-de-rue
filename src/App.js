4ca00e5c95fa75a94817de17e7cb01d14162aa55
Clara Nahornyj <c.nahornyj@hotmail.fr>
2019-12-11 12:49:28 +0100

    Resizing des photos, ajout d'un carousel responsive pour la présentation des tatoueurs permanents

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Home from './components/home'
 import Agency from './components/agency'
 import ButtonOpenPortfolio from './components/ButtonOpenPortfolio';
+import PermanentArtists from './components/permanentArtists';
 
 class App extends Component {
   constructor(props){
@@ -18,6 +19,7 @@ class App extends Component {
         <Header />
         <Home />
         <Agency />
+        <PermanentArtists/>
         <ButtonOpenPortfolio />
       </div>
     );
---
+++ src/components/permanentArtists.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+import alex290 from '../img/tatoueurs/alex290.jpg';
+import cosmic from '../img/tatoueurs/cosmic.jpg';
+import gladys from '../img/tatoueurs/gladys.jpg';
+import timmy_rodger from '../img/tatoueurs/timmy_rodger.jpg';
+
+const items = [
+    {
+      src: `${alex290}`,
+      // altText: 'Slide 1',
+      // caption: 'Slide 1'
+    },
+    {
+      src: `${cosmic}`,
+      // altText: 'Slide 2',
+      // caption: 'Slide 2'
+    },
+    {
+      src: `${gladys}`,
+      // altText: 'Slide 3',
+      // caption: 'Slide 3'
+    },
+    {
+      src: `${timmy_rodger}`,
+      // altText: 'Slide 4',
+      // caption: 'Slide 4'
+    }
+  ];
+  
+  const CarrouselAgency = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+  
+    const next = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const previous = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const goToIndex = (newIndex) => {
+      if (animating) return;
+      setActiveIndex(newIndex);
+    }
+  
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={() => setAnimating(true)}
+          onExited={() => setAnimating(false)}
+          key={item.src}
+        >
+          <img src={item.src} alt={item.altText} />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+  
+    return (
+      <div className="carrouselDiv">
+        <Carousel
+          activeIndex={activeIndex}
+          next={next}
+          previous={previous}
+        >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        </Carousel>
+      </div>
+    );
+  }
+  
+  export default CarrouselAgency;
+
+
