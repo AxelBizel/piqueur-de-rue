37f105a240f54e235e235257a20c5d519c11e969
Cha-dieleman <chadieleman@gmail.com>
2019-12-13 13:17:03 +0100

    animations

--- src/components/modalPortfolio.js
+++ src/components/modalPortfolio.js
@@ -11,17 +11,22 @@ const PortfolioModal = (props) => {
         
         return(
             <>           
-                <div className="firstModal" style={{display : showModal? "flex" : "none"}} onClick={() => props.closeModal()}></div>
+                <div className="firstModalPortfolio" style={{display : showModal? "flex" : "none"}} onClick={() => props.closeModal()}></div>
                 {showModal ? 
                     <aside className="secondModalPortfolio" role="dialog">
-                        <button onClick={ props.closeModal}>X</button>
+                     <div class="close-container" onClick={ props.closeModal}>
+                        <div class="leftright"></div>
+                        <div class="rightleft"></div>
+                        <label class="close" ></label>
+                    </div>
+                        {/* <button className="buttonCloseModal" onClick={ props.closeModal}>X</button> */}
                         <h1>Cosmic Billie</h1>
                         <div className="textEtImg">
                             <a href="https://www.instagram.com/cosmic.billie/?hl=fr"><img className="imgPresentation" src={cosmic} alt="photo de Cosmic Billie"/></a>
                             <a href="https://www.instagram.com/cosmic.billie/?hl=fr"><img src={insta} style={{width:"100px"}}/></a>
                             <p className="textPresentation">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                         </div>
-                        <button>Contacter tatoueur</button>
+                        <button className="buttonPortfolio">Contacter tatoueur</button>
                         <div className="sectionImgs">
                             <img className="imgPrincipale" src={abeille} alt="réalisation du tatoueur" />
                             <CarrouselPortfolio />
