352dd28d244650e44b0b0cc41ba97b904fc2a0eb
Cha-dieleman <chadieleman@gmail.com>
2019-12-13 14:56:10 +0100

    create form

--- src/components/FormulairePortfolio.js
+++ src/components/FormulairePortfolio.js
@@ -1,20 +1,30 @@
 import React, {Component} from 'react';
 
+
 class FormulairePortfolio extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state= {};
+        this.state = {
+            title : "Cosmic Billie"
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+      }
+
+    handleChange(event){
+        this.setState({ title : event.target.value });
     }
 
     render(){
-        
-        return(
-            <div className="SectionContainer">
-                <h1 className="HeaderSection">NOS TATOUEURS</h1>
-                <button style={styleButton} onClick={this.openModal}>Accéder à son portfolio</button>
-                <PortfolioModal showModal={this.state.showModal} closeModal={this.closeModal} />
-            </div>
-        )
+        return (
+            <form className="formPortfolio" onSubmit={event => event.preventDefault()}>
+                <input name="fullName" type="text" onChange={this.handleChange} value={this.state.title}></input>
+                <h1>{this.state.title}</h1>
+            </form>
+        );
     }
 }
 
