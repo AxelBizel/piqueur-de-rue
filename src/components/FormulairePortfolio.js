2ce8ee51873aac69541800228884ac75b4aa7c05
Cha-dieleman <chadieleman@gmail.com>
2019-12-16 11:44:47 +0100

    construction of the form

--- src/components/FormulairePortfolio.js
+++ src/components/FormulairePortfolio.js
@@ -4,18 +4,32 @@ import React, {Component} from 'react';
 class FormulairePortfolio extends Component {
     constructor(props){
         super(props);
-        this.state = {value : ""};
+        this.state = {  valueFullname : "",
+                        valueAge : null,
+                        valueTel : null,
+                        valueEmail : null,
+                        valueTattooLocation : "",
+                        };
     }
 
 
 
     handleChange = (event) =>{
-    this.setState({value: event.target.value});
+        const { valueFullname, valueAge, valueTel, valueEmail } = this.state;
+        this.state.valueFullname = event.target.valueFullname;
+        this.state.valueAge = event.target.valueAge;
+        this.state.valueTel = event.target.valueTel;
+        this.state.valueAge = event.target.valueAge;
+        this.state.valueTattooLocation = event.target.valueTattooLocation;
+        this.setState({ valueFullname,
+                        valueAge,
+                        valueTel,
+                        valueEmail });
     }
 
     handleSubmit = (event) =>{
-    alert('Le nom a été soumis : ' + this.state.value);
-    event.preventDefault();
+        alert('Le nom a été soumis : ' + this.state.value);
+        event.preventDefault();
     }
     
     
@@ -24,7 +38,30 @@ class FormulairePortfolio extends Component {
         return (
             <form className="formPortfolio" onSubmit={event => event.preventDefault()}>
                 <h1>Formulaire de contact :</h1>
-                <input name="fullName" type="text" onChange={this.handleChange} value={this.state.value} placeholder="Votre nom, prénom"></input>
+                <h2>Vous :</h2>
+
+                <input name="fullName" type="text" onChange={this.handleChange} valueFullname={this.state.valueFullname} placeholder="Votre nom, prénom :"  required></input>
+
+                <input name="age" type="number" onChange={this.handleChange} valueAge={this.state.valueAge} placeholder="Votre âge :"  required></input>
+
+                <input name="phone" type="tel" onChange={this.handleChange} valueTel={this.state.valueTel} size={10} minlength={1} maxlength={10} pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" placeholder="Votre numéro de téléphone :"  required></input>
+                  
+                <input name="email" type="email" onChange={this.handleChange} valueEmail={this.state.valueEmail} placeholder="Votre adresse mail :" required></input>
+                
+                <h2>Votre projet :</h2>
+
+                <input name="tattoolocation" type="text" onChange={this.handleChange} valueTattooLocation={this.state.valueTatooLocation} placeholder="Emplacement du tatouage souhaitée :"  required></input>
+
+                <input name="dimension" type="number" onChange={this.handleChange} valueDimension={this.state.valueDimension} placeholder="La dimension du tatouage souhaité en centimètres :"  required></input>
+
+                <input name="budget" type="number" onChange={this.handleChange} valueBudget={this.state.valueBudget} placeholder="Votre budget :"  required></input>
+
+                <textarea id="story" name="story" rows="5" cols="33">
+                Dîtes-nous en plus sur votre projet ici :
+                </textarea>
+
+                <label id="labelPhone">Pourquoi avons-nous besoin de ces informations ?</label>
+
             </form>
         );
     }
