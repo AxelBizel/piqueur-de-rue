97bed79ee44bf7b5ab1df5d9c0c7365343849b27
Axel Bizel <axel.bizel@gmail.com>
2019-12-16 14:45:05 +0100

    debuts inserts portfolios

--- src/back/sql/init.sql
+++ src/back/sql/init.sql
@@ -94,3 +94,10 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+
+-- -----------------------------------------------------
+-- Ajout champ "style" dans table portfolio
+-- -----------------------------------------------------
+ALTER TABLE portfolio ADD style TEXT;
--- src/back/sql/insert.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO portfolio (pseudo, type, presentation, active) VALUES ('toto', 'team', 'blablblablbalba', '1');
