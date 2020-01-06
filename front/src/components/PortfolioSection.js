1761aa610077c8e617952c2c3ed1663fc49ca303
Axel Bizel <axel.bizel@gmail.com>
2020-01-06 15:54:47 +0100

    ajout boutons modal portfolio

--- front/src/components/PortfolioSection.js
+++ front/src/components/PortfolioSection.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import ButtonOpenPortfolio from "./ButtonOpenPortfolio";
 
- class PortfolioSection extends Component {
+class PortfolioSection extends Component {
   state = {
     portfolios: null
-  }
+  };
 
   componentDidMount() {
-    axios.get(`http://localhost:5000/api/portfolio`)
-      .then(res => {
-          const portfoliosData = res.data
-        this.setState({ portfolios: portfoliosData });
-        console.log(this.state)
-      })
+    axios.get(`http://localhost:5000/api/portfolio`).then(res => {
+      const portfoliosData = res.data;
+      this.setState({ portfolios: portfoliosData });
+      console.log(this.state);
+    });
   }
 
   render() {
     return (
       <div>
-        {this.state.portfolios === null
-        ? <p>loading</p>
-        : this.state.portfolios.map(portfolio => <div style={{widht:'100px', height:'100px'}}><img src={portfolio.photo}/></div>)}
+        {this.state.portfolios === null ? (
+          <p>loading</p>
+        ) : (
+          this.state.portfolios.map(portfolio => (
+            <div style={{ widht: "200px", height: "200px" }}>
+              <ul>
+                <li>{portfolio.pseudo}</li>
+              </ul>
+              <ButtonOpenPortfolio />
+              <img src={portfolio.photo} />
+            </div>
+          ))
+        )}
       </div>
-    )
+    );
   }
 }
 
