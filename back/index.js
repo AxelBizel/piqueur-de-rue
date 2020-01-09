fbf5585f0390963e2311c263ef40f764caeb9158
Morgane Mottey <morgane.mottey@gmail.com>
2020-01-09 10:25:32 +0100

    token ok

--- back/index.js
+++ back/index.js
@@ -3,67 +3,124 @@ const app = express();
 const port = 5000;
 const connection = require("./conf");
 const bodyParser = require("body-parser");
+const path = require ('path');
+const session = require ('express-session');
+const jwt = require ('jsonwebtoken')
+
 // const cors = require("cors");
 
 // bodyParser
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.use(express.json())
+
+
+//Authetification avec Express-Session 
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname + '/login.html'));
+  console.log(request.headers.authorization)
+  // const user = {}
+  // response.json(user)
+});
+
+app.post('/auth', function(request, response) {
+  const login = request.body.login;
+	const password = request.body.password;
+	if (login && password) {
+		connection.query('SELECT * FROM users WHERE login = ? AND password = ?', [login, password], function(error, results, fields) {
+      console.log ("result",results.length)
+      if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.login = login;
+        jwt.sign ({
+          user : {
+            id : results[0].id,
+            login : results[0].login,
+          } 
+        }, 
+        require('./conf').secret , 
+        (err,token) => {
+          console.log(err, token)
+          response.json({token})
+        })
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Bienvenue, ' + request.session.login + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+  // //Cors
+  // app.use(cors());
+  // app.get("/products/:id", function(req, res, next) {
+  //   res.json({ msg: "This is CORS-enabled for all origins!" });
+  // });
+
+  // ROUTES
+
+  //Récupération des users
+  app.get("/api/users", (req, res) => {
+    connection.query("SELECT * from users", (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération des portfolios");
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  //Récupération des datas portfolios
+
+  app.get("/api/portfolio", (req, res) => {
+    connection.query("SELECT * from portfolio", (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération des portfolios");
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  //Récupération des images
+  app.get("/api/images", (req, res) => {
+    connection.query("SELECT * from images", (err, results) => {
+      if (err) {
+        res.status(500).send("Erreur lors de la récupération des portfolios");
+      } else {
+        res.json(results);
+      }
+    });
+  });
+
+  //Server
+  app.get("/", (request, response) => {
+    response.send("Bienvenue sur Express");
+  });
+
+  app.listen(port, err => {
+    if (err) {
+      throw new Error("Something bad happened...");
+    }
+
+    console.log(`Server is listening on ${port}`);
   })
-);
-
-// //Cors
-// app.use(cors());
-// app.get("/products/:id", function(req, res, next) {
-//   res.json({ msg: "This is CORS-enabled for all origins!" });
-// });
-
-// ROUTES
-
-//Récupération des users
-app.get("/api/users", (req, res) => {
-  connection.query("SELECT * from users", (err, results) => {
-    if (err) {
-      res.status(500).send("Erreur lors de la récupération des portfolios");
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-//Récupération des datas portfolios
-
-app.get("/api/portfolio", (req, res) => {
-  connection.query("SELECT * from portfolio", (err, results) => {
-    if (err) {
-      res.status(500).send("Erreur lors de la récupération des portfolios");
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-//Récupération des images
-app.get("/api/images", (req, res) => {
-  connection.query("SELECT * from images", (err, results) => {
-    if (err) {
-      res.status(500).send("Erreur lors de la récupération des portfolios");
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-//Server
-app.get("/", (request, response) => {
-  response.send("Bienvenue sur Express");
-});
-
-app.listen(port, err => {
-  if (err) {
-    throw new Error("Something bad happened...");
-  }
-
-  console.log(`Server is listening on ${port}`);
-});
--- back/sql/insertUsersAndPortfolios.sql
+++ back/sql/insertUsersAndPortfolios.sql
@@ -3,3 +3,12 @@ VALUES ('Timmy Rodger', 'team', 'Issu d''une formation de designer produit et 
 INSERT INTO portfolio (pseudo, type, presentation, style, active, photo) VALUES ('Cosmic Billie', 'team', 'Marianne alias COSMIC BILLIE, passionnée d''art et de culture indienne. Initiée très jeune au milieu du tatouage, elle a su tester plusieurs styles avant d''approfondir sa personnalité artistique. A l''écoute, souriante et soignée, elle sera ravie de vous faire découvrir son univers floral et esthétique.', 'Hindi et Ornemental', '1', 'src/img/tatoueurs/cosmic.jpg');
 INSERT INTO portfolio (pseudo, type, presentation, style, active, photo) VALUES ('Gladystomeetyou', 'team', 'Agée de 22 ans, Gladys a su nourrir son univers graphique depuis sa tendre enfance. Son cursus dans le monde de l''art lui a permis de s''ouvrir à différents modes d''expression, de collaborer à des projets divers et d''exposer à plusieurs reprises. Précoce dans le milieu du tatouage, elle a su proposer son univers naïf aux lignes épurées.', 'Graphique, Naïf, épuré...', '1', 'src/img/tatoueurs/gladys.jpg');
 INSERT INTO portfolio (pseudo, type, presentation, style, active, photo) VALUES ('Alex 290', 'team', 'Tombé dans le tatouage grâce à la musique, j''ai remonté le fil des références jusqu''aux sources du traditionnel occidental, qui est aujourd''hui mon domaine de prédilection.', 'Traditionnel Occidental', '1', 'src/img/tatoueurs/alex290.jpg');
+
+
+
+--Insert Users // Test JWT
+
+INSERT INTO users (login , password) VALUES ('Timmy Rodger', 'timmyrodger1900'),
+INSERT INTO users (login , password) VALUES ('Cosmic Billie', 'cosmicbillie1960'),
+INSERT INTO users (login , password) VALUES ('Galdysmeetyou', 'gladysmeetyou1990'),
+INSERT INTO users (login , password) VALUES ('Alex 290', 'alex2901010');
