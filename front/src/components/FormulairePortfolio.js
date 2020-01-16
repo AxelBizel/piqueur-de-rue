774d6e78cfb3d2703d78c5bf77fcca1e330a4c01
Cha-dieleman <chadieleman@gmail.com>
2020-01-16 15:30:20 +0100

    essai css form

--- front/src/components/FormulairePortfolio.js
+++ front/src/components/FormulairePortfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './formulaireChacha.css';
 const axios = require('axios');
 
 
@@ -46,21 +47,21 @@ class FormulairePortfolio extends Component {
     render() {
         return (
             <>
-                <form className="formPortfolio" onSubmit={this.handleSubmit} method="POST" action='/api/customers'>
-                    <h1>Formulaire de contact :</h1>
+                <form className="formCha" onSubmit={this.handleSubmit} method="POST" action='/api/customers'>
+                    <h1 className="h1formCha">- Formulaire de contact -</h1>
 
-                    <h2>Vous :</h2>
-                    <input name="firstname" type="text" onChange={this.handleChange} value={this.state.firstname} placeholder="Votre prénom :" required></input>
-                    <input name="lastname" type="text" onChange={this.handleChange} value={this.state.lastname} placeholder="Votre nom :" required></input>
-                    <input name="age" type="number" onChange={this.handleChangeInteger} value={this.state.age} placeholder="Votre âge :" required></input>
-                    <input name="phone" type="tel" onChange={this.handleChange} value={this.state.phone} size={10} minLength={1} maxLength={10} placeholder="Votre numéro de téléphone :" required></input>
-                    <input name="email" type="email" onChange={this.handleChange} value={this.state.email} placeholder="Votre adresse mail :" required></input>
+                    <h2 className="h2formCha">Vous :</h2>
+                    <input className="inputCha" name="firstname" type="text" onChange={this.handleChange} value={this.state.firstname} placeholder="Votre prénom :" required></input>
+                    <input className="inputCha" name="lastname" type="text" onChange={this.handleChange} value={this.state.lastname} placeholder="Votre nom :" required></input>
+                    <input className="inputCha" name="age" type="number" onChange={this.handleChangeInteger} value={this.state.age} placeholder="Votre âge :" required></input>
+                    <input className="inputCha" name="phone" type="tel" onChange={this.handleChange} value={this.state.phone} size={10} minLength={1} maxLength={10} placeholder="Votre numéro de téléphone :" required></input>
+                    <input className="inputCha" name="email" type="email" onChange={this.handleChange} value={this.state.email} placeholder="Votre adresse mail :" required></input>
 
-                    <h2>Votre projet :</h2>
-                    <input name="tattoolocation" type="text" onChange={this.handleChange} value={this.state.tattoolocation} placeholder="Emplacement du tatouage souhaitée :" ></input>
-                    <input name="hauteur" type="number" onChange={this.handleChangeInteger} value={this.state.hauteur} placeholder="Dimension de tatouage souhaitée (hauteur en centimètres):" ></input>
-                    <input name="largeur" type="number" onChange={this.handleChangeInteger} value={this.state.largeur} placeholder="Dimension de tatouage souhaitée (largeur en centimètres):" ></input>
-                    <input name="budget" type="number" onChange={this.handleChangeInteger} value={this.state.budget} placeholder="Votre budget :" ></input>
+                    <h2 className="h2formCha">Votre projet :</h2>
+                    <input className="inputCha" name="tattoolocation" type="text" onChange={this.handleChange} value={this.state.tattoolocation} placeholder="Emplacement du tatouage souhaitée :" ></input>
+                    <input className="inputCha" name="hauteur" type="number" onChange={this.handleChangeInteger} value={this.state.hauteur} placeholder="Dimension de tatouage souhaitée (hauteur en centimètres):" ></input>
+                    <input className="inputCha" name="largeur" type="number" onChange={this.handleChangeInteger} value={this.state.largeur} placeholder="Dimension de tatouage souhaitée (largeur en centimètres):" ></input>
+                    <input className="inputCha" name="budget" type="number" onChange={this.handleChangeInteger} value={this.state.budget} placeholder="Votre budget :" ></input>
                     <textarea id="story" name="story" rows="5" cols="33" onChange={this.handleChange} value={this.state.story} placeholder="Dîtes-nous en plus ici : ">
                     </textarea>
                     <label id="labelPhone">Pourquoi avons-nous besoin de ces informations ?</label>
