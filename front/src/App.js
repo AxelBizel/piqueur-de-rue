080d15e91c2f1dad372e91a4b5b48119979e0001
Signupz <ludovic.mazet@gmail.com>
2020-01-16 17:10:54 +0100

    mise en forme du menu login

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import AOS from 'aos'
 import Navbar from "./components/navbar";
 import Header from "./components/header";
 import Home from "./components/home";
@@ -8,17 +7,12 @@ import Footer from "./components/Footer";
 import GuestHome from "./components/GuestHome";
 import PortfolioSection from "./components/PortfolioSection";
 import YourProjectAccordion from "./components/YourProjectAccordion";
-import 'aos/dist/aos.css';
 import "./index.css";
+import 'aos/dist/aos.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-AOS.init({
-  mirror: true,
-  duration: 600
-})
 
-window.addEventListener('load', AOS.refresh);
 
 class App extends Component {
   constructor(props) {
@@ -41,9 +35,6 @@ class App extends Component {
         <div id="guests">
           <GuestHome />
         </div>
-        {/* <div id="yourproject">
-          <YourProject />
-        </div> */}
         <div id="yourproject">
           <YourProjectAccordion />
         </div>
--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -45,7 +45,7 @@ export default function Footer() {
                 <iframe
                   className="map"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.248727350261!2d4.816656050991151!3d45.76620532115945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb08712c38c3%3A0x923df72aae138019!2sPiqueur%20de%20rue!5e0!3m2!1sfr!2sfr!4v1576071247997!5m2!1sfr!2sfr"
-                  data-aos="fade-left"  data-aos-duration="500"></iframe>
+                  data-aos="fade-left" data-aos-duration="500"></iframe>
             </Col>
           </Row>
         </Container>
