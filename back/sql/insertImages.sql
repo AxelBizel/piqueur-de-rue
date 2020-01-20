8bd868e18752605002641ca7a3dd33586d1ae0ed
Axel Bizel <axel.bizel@gmail.com>
2020-01-20 09:44:31 +0100

    pre merge dev

---
+++ back/sql/insertImages.sql
@@ -0,0 +1,142 @@
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES ('A290_1',1,'src/img_piqueurs/tatoueurs/alex_290/A290_1.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES ('A290_2',1,'src/img_piqueurs/tatoueurs/alex_290/A290_2.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_3',1,'src/img_piqueurs/tatoueurs/alex_290/A290_3.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_4',1,'src/img_piqueurs/tatoueurs/alex_290/A290_4.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_5',1,'src/img_piqueurs/tatoueurs/alex_290/A290_5.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_6',1,'src/img_piqueurs/tatoueurs/alex_290/A290_6.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_7',1,'src/img_piqueurs/tatoueurs/alex_290/A290_7.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_8',1,'src/img_piqueurs/tatoueurs/alex_290/A290_8.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_9',1,'src/img_piqueurs/tatoueurs/alex_290/A290_9.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_10',1,'src/img_piqueurs/tatoueurs/alex_290/A290_10.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_10',1,'src/img_piqueurs/tatoueurs/alex_290/A290_11.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_11',1,'src/img_piqueurs/tatoueurs/alex_290/A290_12.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_12',1,'src/img_piqueurs/tatoueurs/alex_290/A290_13.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_13',1,'src/img_piqueurs/tatoueurs/alex_290/A290_14.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_14',1,'src/img_piqueurs/tatoueurs/alex_290/A290_15.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('A290_profile',1,'src/img_piqueurs/tatoueurs/alex_290/alex_290.jpg',4);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_1',1,'src/img_piqueurs/tatoueurs/cosmic_billie/__CB1.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_2',1,'src/img_piqueurs/tatoueurs/cosmic_billie/__CB2.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_3',1,'src/img_piqueurs/tatoueurs/cosmic_billie/__CB3.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_4',1,'src/img_piqueurs/tatoueurs/cosmic_billie/__CB4.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_5',1,'src/img_piqueurs/tatoueurs/cosmic_billie/__CB5.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_6',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB1.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_7',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB2.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_8',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB3.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_9',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB4.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_10',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB5.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_11',1,'src/img_piqueurs/tatoueurs/cosmic_billie/CB6.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('cosmic_billie_portrait',1,'src/img_piqueurs/tatoueurs/cosmic_billie/cosmic_billie.jpg',2);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_1',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G13.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_2',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G14.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_3',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G15.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_4',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G16.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_5',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G17.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_6',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G18.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_7',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G19.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_8',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G20.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_9',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G21.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_10',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G22.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_11',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G23.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_12',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G24.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_13',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G25.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_14',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G26.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_15',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G27.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_16',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G28.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_17',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G29.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_18',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G30.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_19',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G31.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_20',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G32.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyou_21',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/G33.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('gladystomeetyouportrait',1,'src/img_piqueurs/tatoueurs/gladystomeetyou/gladystomeetyou.jpg', 3);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_1',1,'src/img_piqueurs/tatoueurs/timmy_rodger/BateauPirate_insta.jpg');
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_2',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Coyotte_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_3',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Coyotte.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_4',1,'src/img_piqueurs/tatoueurs/timmy_rodger/DragonHead.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_5',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Indienne.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_6',1,'src/img_piqueurs/tatoueurs/timmy_rodger/LanterneSkull_healed.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_7',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Luna_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_8',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Luna.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_9',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Lune_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_10',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Lune.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_11',1,'src/img_piqueurs/tatoueurs/timmy_rodger/NeoNico_color_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_12',1,'src/img_piqueurs/tatoueurs/timmy_rodger/NeoNico_color.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_13',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Neptune_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_14',1,'src/img_piqueurs/tatoueurs/timmy_rodger/OwlBlinder_color_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_15',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Pin_upTrad.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_16',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Pirate.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_17',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Reaper.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_18',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Sphinx.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_19',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Stitch_gp.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_20',1,'src/img_piqueurs/tatoueurs/timmy_rodger/Stitch.jpg', 1);
+INSERT INTO images (alt_text , active , path , portfolio_id) 
+VALUES('timmyrodger_portrait',1,'src/img_piqueurs/tatoueurs/timmy_rodger/timmy_rodger.jpg', 1);
---
+++ back/sql/insertUsers.sql
@@ -0,0 +1,21 @@
+--Insert Users // Test JWT
+
+INSERT INTO users
+    (login , password)
+VALUES
+    ('TimmyRodger', 'timmyrodger')
+,
+INSERT INTO users
+    (login , password)
+VALUES
+    ('CosmicBillie', 'cosmicbillie')
+,
+INSERT INTO users
+    (login , password)
+VALUES
+    ('Gladysmeetyou', 'gladysmeetyou')
+,
+INSERT INTO users
+    (login , password)
+VALUES
+    ('Alex290', 'alex290');
