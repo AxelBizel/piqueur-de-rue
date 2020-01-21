0ab84bc230d5f80ec2749a7f9319803e76d1858d
Morgane Mottey <morgane.mottey@gmail.com>
2020-01-21 11:52:30 +0100

    buttonportofolio modifié

--- front/src/components/ButtonAdminPortfolio.js
+++ front/src/components/ButtonAdminPortfolio.js
@@ -1,42 +1,3 @@
-// import React, { useState } from 'react';
-// import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-// const ButtonAdminPortfolio = (props) => {
-//   const {
-//     buttonLabel,
-//     className
-//   } = props;
-
-//   const [modal, setModal] = useState(false);
-
-//   const toggle = () => {
-//     props.getProfile();
-//     setModal(!modal);
-//   }
-
-//   return (
-//     <div>
-//       <Button color="danger" onClick={toggle}>{props.children}</Button>
-//       <Modal isOpen={modal} fade={false} toggle={toggle} className={className}>
-//         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-//         <ModalBody>
-//          {props.portfolio.pseudo}
-//         </ModalBody>
-//         <ModalFooter>
-//           <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-//           <Button color="secondary" onClick={toggle}>Cancel</Button>
-//         </ModalFooter>
-//       </Modal>
-//     </div>
-//   );
-// }
-
-// export default ButtonAdminPortfolio;
-
-
-
-
-
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
@@ -61,6 +22,10 @@ const ButtonAdminPortfolio = (props) => {
         <ModalHeader toggle={toggle}>Modal title</ModalHeader>
         <ModalBody>
          {props.portfolio.pseudo}
+        </ModalBody>
+        {props.portfolio.type}
+        <ModalBody>
+         {props.portfolio.presentation}
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
