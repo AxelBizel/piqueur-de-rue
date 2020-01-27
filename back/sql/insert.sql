86cc5788aa95ce1f31a3e14286c0a8070a70d37d
Axel Bizel <axel.bizel@gmail.com>
2020-01-27 16:36:42 +0100

    remplacement fichiers img votre projet + insert grozen en bdd

--- back/sql/insert.sql
+++ back/sql/insert.sql
@@ -15,20 +15,26 @@ VALUES ('Alex290', 'alex290', 1);
 
 
 
-INSERT INTO portfolio (pseudo, type, presentation, style, active, photo, insta) 
-VALUES ('Timmy Rodger', 'team', 'Issu d''une formation de designer produit et graphique, il a acquis une forte expérience à travers la création de nombreux projets dans des secteurs graphiques tels que la mode, la communication visuelle, la publication de magazines, le design produit et architecture d''intérieur. Il met ses nombreuses expériences au service du milieu du tatouage en ciblant au mieux le souhait des clients.', 'Semi réaliste, Néo trad...', '1', '/back/img/photos_tatoueurs/timmy_rodger.jpg', 'https://www.instagram.com/timmyrodger/');
+INSERT INTO portfolio (pseudo, type, presentation, style, active,  insta) 
+VALUES ('Timmy Rodger', 'team', 'Issu d''une formation de designer produit et graphique, il a acquis une forte expérience à travers la création de nombreux projets dans des secteurs graphiques tels que la mode, la communication visuelle, la publication de magazines, le design produit et architecture d''intérieur. Il met ses nombreuses expériences au service du milieu du tatouage en ciblant au mieux le souhait des clients.', 'Semi réaliste, Néo trad...', '1', 'https://www.instagram.com/timmyrodger/');
 
-INSERT INTO portfolio (pseudo, type, presentation, style, active, photo, insta) 
-VALUES ('Cosmic Billie', 'team', 'Marianne alias COSMIC BILLIE, passionnée d''art et de culture indienne. Initiée très jeune au milieu du tatouage, elle a su tester plusieurs styles avant d''approfondir sa personnalité artistique. A l''écoute, souriante et soignée, elle sera ravie de vous faire découvrir son univers floral et esthétique.', 'Hindi et Ornemental', '1', '/back/img/photos_tatoueurs/cosmic.jpg', 'https://www.instagram.com/cosmic.billie/');
+INSERT INTO portfolio (pseudo, type, presentation, style, active,  insta) 
+VALUES ('Cosmic Billie', 'team', 'Marianne alias COSMIC BILLIE, passionnée d''art et de culture indienne. Initiée très jeune au milieu du tatouage, elle a su tester plusieurs styles avant d''approfondir sa personnalité artistique. A l''écoute, souriante et soignée, elle sera ravie de vous faire découvrir son univers floral et esthétique.', 'Hindi et Ornemental', '1',  'https://www.instagram.com/cosmic.billie/');
 
-INSERT INTO portfolio (pseudo, type, presentation, style, active, photo, insta)
-VALUES ('Gladystomeetyou', 'team', 'Agée de 22 ans, Gladys a su nourrir son univers graphique depuis sa tendre enfance. Son cursus dans le monde de l''art lui a permis de s''ouvrir à différents modes d''expression, de collaborer à des projets divers et d''exposer à plusieurs reprises. Précoce dans le milieu du tatouage, elle a su proposer son univers naïf aux lignes épurées.', 'Graphique, Naïf, épuré...', '1', '/back/img/photos_tatoueurs/gladys.jpg', 'https://www.instagram.com/gladystomeetyou/');
+INSERT INTO portfolio (pseudo, type, presentation, style, active, insta)
+VALUES ('Gladystomeetyou', 'team', 'Agée de 22 ans, Gladys a su nourrir son univers graphique depuis sa tendre enfance. Son cursus dans le monde de l''art lui a permis de s''ouvrir à différents modes d''expression, de collaborer à des projets divers et d''exposer à plusieurs reprises. Précoce dans le milieu du tatouage, elle a su proposer son univers naïf aux lignes épurées.', 'Graphique, Naïf, épuré...', '1',  'https://www.instagram.com/gladystomeetyou/');
 
-INSERT INTO portfolio (pseudo, type, presentation, style, active, photo, insta)
-VALUES ('Alex 290', 'team', 'Tombé dans le tatouage grâce à la musique, j''ai remonté le fil des références jusqu''aux sources du traditionnel occidental, qui est aujourd''hui mon domaine de prédilection.', 'Traditionnel Occidental', '1', '/back/img/photos_tatoueurs/alex290.jpg', 'https://www.instagram.com/alex290_tattoo/');
+
+INSERT INTO portfolio (pseudo, type, presentation, style, active, insta)
+VALUES ('Alex 290', 'team', 'Tombé dans le tatouage grâce à la musique, j''ai remonté le fil des références jusqu''aux sources du traditionnel occidental, qui est aujourd''hui mon domaine de prédilection.', 'Traditionnel Occidental', '1', 'https://www.instagram.com/alex290_tattoo/');
+
 
 INSERT INTO portfolio (pseudo, type, presentation, active, insta, startdate, enddate)
 VALUES ('Fernando Madeira', 'guest', 'Artiste Brésilien spécialisé dans les tatouages aquarelle.', '1', 'https://www.instagram.com/fernandomadeira/?hl=fr', '7 février', '16 février');
+
+
+INSERT INTO portfolio (pseudo, type, presentation, style, active, insta)
+VALUES ('Grozen', 'team', "Dorian alias GROZEN est un tatoueur passionné et méticuleux prêt a travailler sur vos projets les plus extravagants et graphique. Amoureux de grosses lignes et petits points, il aura toujours coeur d'être à votre écoute.", 'Mandala, paternes géométriques, dot work', '1', 'https://www.instagram.com/alex290_tattoo/');
 
 
 
@@ -192,3 +198,29 @@ INSERT INTO images (alt_text , active , path , portfolio_id, type)
 VALUES('FM9',1,'http://localhost:5000/img/5/FM9.jpg', 5, 'realisation');
 INSERT INTO images (alt_text , active , path , portfolio_id, type) 
 VALUES('portrait',0,'http://localhost:5000/img/5/portrait.jpg', 5, 'avatar');
+
+
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen1',1,'http://localhost:5000/img/6/grozen1.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen2',1,'http://localhost:5000/img/6/grozen2.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen3',1,'http://localhost:5000/img/6/grozen3.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen4',1,'http://localhost:5000/img/6/grozen4.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen5',1,'http://localhost:5000/img/6/grozen5.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen6',1,'http://localhost:5000/img/6/grozen6.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen7',1,'http://localhost:5000/img/6/grozen7.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen8',1,'http://localhost:5000/img/6/grozen8.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen9',1,'http://localhost:5000/img/6/grozen9.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen10',1,'http://localhost:5000/img/6/grozen10.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('grozen11',1,'http://localhost:5000/img/6/grozen11.jpeg', 6, 'realisation');
+INSERT INTO images (alt_text , active , path , portfolio_id, type) 
+VALUES('portrait de grozen',0,'http://localhost:5000/img/6/portrait', 6, 'portrait');
