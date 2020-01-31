c9fd56d88a054ed85a3c38caf50c7dac9b3c9eb9
Axel Bizel <axel.bizel@gmail.com>
2020-01-31 09:28:50 +0100

    changement conf.js

--- back/conf.js
+++ back/conf.js
@@ -1,18 +1,9 @@
+// Conf.js
 const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost", // adresse du serveur
     user: "root", // le nom d'utilisateur
-    password: 'Ivo13122018', // le mot de passe
+    password: "Ivo13122018", // le mot de passe
     database: "piqueurderue" // le nom de la base de données
 });
-
-const IPserver = "http://localhost:5000";
-const portHttp = 5000;
-// const portHttps = null;
-
-module.exports = {
-    connection,
-    IPserver,
-    portHttp,
-};
-
+module.exports = connection;
